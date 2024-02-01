b811feb1f6a13a4f468ea3b6d8413b2f284dd6d2
602822 <89077126+602822@users.noreply.github.com>
2024-02-01 10:48:08 +0100

    Made the list of dogs available to both Activities

---
+++ app/src/main/java/com/example/oblig1quizapp/DogList.java
@@ -0,0 +1,17 @@
+package com.example.oblig1quizapp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DogList
+{
+    public static List<Dog> dogs = new ArrayList<>();
+
+    static {
+        dogs.add(new Dog(R.drawable.goldenretriever, "Golden Retriever"));
+        dogs.add(new Dog(R.drawable.husky, "Husky"));
+        dogs.add(new Dog(R.drawable.labradorretriever, "Labrador Retriever"));
+    }
+
+
+}
