d7b7a884ebcb8a28cf8a1f0c532e23b5db0780f0
602822 <89077126+602822@users.noreply.github.com>
2024-03-21 11:11:32 +0100

    Did some code refactoring

--- app/src/main/java/com/example/oblig1quizapp/QuizActivity.java
+++ app/src/main/java/com/example/oblig1quizapp/QuizActivity.java
@@ -1,8 +1,11 @@
 package com.example.oblig1quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.StrictMode;
@@ -11,30 +14,28 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class QuizActivity extends AppCompatActivity {
 
-    QuizViewModel quizViewModel;
+    static QuizViewModel quizViewModel;
 
+    Button button;
+    Button button2;
+    Button button3;
+
+    TextView answerView;
+
+    ImageView dogImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
-                .detectDiskReads()
-                .detectDiskWrites()
-                .detectNetwork()   // or .detectAll() for all detectable problems
-                .penaltyLog()
-                .detectAll()
-                .build());
 
-        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
-                .detectLeakedSqlLiteObjects()
-                .detectLeakedClosableObjects()
-                .penaltyLog()
-                .penaltyDeath()
-                .detectAll()
-                .build());
         super.onCreate(savedInstanceState);
 
 
@@ -48,9 +49,15 @@ public class QuizActivity extends AppCompatActivity {
         //Assosiate the ViewModel with the activity
         quizViewModel = new ViewModelProvider(this).get(QuizViewModel.class);
 
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        answerView = findViewById(R.id.correctAnswerOrNot);
+        dogImageView = findViewById(R.id.quizDogImage);
+
+
         //Sets initial score and updated score when it changes
         quizViewModel.getScore().observe(this, score -> {
-            Log.d("quizDebug", "The score changed! Score: " + score);
             if (score != null) {
                 TextView scoreView = findViewById(R.id.scoreText);
                 scoreView.setText("Your Score: " + score);
@@ -59,7 +66,6 @@ public class QuizActivity extends AppCompatActivity {
 
         //Sets initial rounds played and updated rounds played when it changes
         quizViewModel.getRoundsplayed().observe(this, roundsPlayed -> {
-            Log.d("quizDebug", "The rounds played changed! Rounds played: " + roundsPlayed);
             if (roundsPlayed != null) {
                 TextView rounds = findViewById(R.id.rounds);
                 String roundText = "Round: " + roundsPlayed;
@@ -68,21 +74,199 @@ public class QuizActivity extends AppCompatActivity {
         });
 
 
-        ImageView dogImageView = findViewById(R.id.quizDogImage);
-        Button button = findViewById(R.id.button);
-        Button button2 = findViewById(R.id.button2);
-        Button button3 = findViewById(R.id.button3);
-        TextView answerView = findViewById(R.id.correctAnswerOrNot);
-
-
-        //Logic for initial state of the game
         //will only start the quiz once i have access to the dog objects
         quizViewModel.getAllDogs().observe(this, dogs -> {
-            quizViewModel.play(dogImageView, button, button2, button3, this);
+
+            //Logic for initial state of the game
+            play();
+
             //The buttons will now call the checkAnswer and playAgain methods when clicked
-            quizViewModel.setButtonClickListeners(dogs, button, button2, button3, answerView, dogImageView, this);
+            setButtonClickListeners(dogs);
         });
 
+
+    }
+
+
+    public void setButtonClickListeners(List<DogEntity> dogs) {
+
+
+        button.setOnClickListener(v ->
+        {
+
+            checkAnswer(button);
+            playAgain(dogs);
+
+        });
+
+        button2.setOnClickListener(v ->
+        {
+            checkAnswer(button2);
+            playAgain(dogs);
+
+        });
+
+        button3.setOnClickListener(v ->
+        {
+            checkAnswer(button3);
+            playAgain(dogs);
+
+        });
+    }
+
+    public void checkAnswer(Button button) {
+
+
+        MutableLiveData<Integer> score = quizViewModel.getScore();
+
+        MutableLiveData<DogEntity> dogPicked = quizViewModel.getDogPicked();
+
+        DogEntity dog = dogPicked.getValue();
+
+        if (button.getText().equals(dog.getImageText())) {
+            answerView.setText("Correct Answer!");
+
+            int currentScore = score.getValue() != null ? score.getValue() : 0;
+
+
+            score.setValue(currentScore + 1);
+
+            Log.d("quizDebug", "Score: " + currentScore);
+
+        } else {
+            answerView.setText("The correct answer was: " + dog.getImageText());
+        }
+    }
+
+    public void playAgain(List<DogEntity> dogs) {
+
+
+        pickRandomDog(dogs);
+
+        MutableLiveData<DogEntity> dogPicked = quizViewModel.getDogPicked();
+
+        DogEntity dog = dogPicked.getValue();
+
+        MutableLiveData<Integer> roundsPlayed = quizViewModel.getRoundsplayed();
+
+        int roudsPlayedValue = roundsPlayed.getValue();
+
+
+        roundsPlayed.setValue(roudsPlayedValue + 1);
+
+
+        //Set the Dog image
+        if (dog.getImageResource() != 0) {
+            //  dogImageView.setImageResource(dog.getImageResource()); //setImageResource reads from disk and will bottleneck the app
+            Glide.with(this).load(dog.getImageResource()).into(dogImageView); // use glide to load image from resource async
+        } else if (dog.getImageUri() != null) {
+            //  dogImageView.setImageURI(imageUri);  // seImageUri reads from disk and will bottleneck the app
+            Glide.with(this).load(dog.getImageUri()).into(dogImageView); // use glide to load image from uri async
+        } else {
+            Log.d("test", "No image URI or imageResource");
+        }
+
+
+        //Update the buttons
+        fillButtonOptionsList(dogs);
+        addTextToButtons();
+
+    }
+
+
+    public static void fillButtonOptionsList(List<DogEntity> dogs) {
+
+
+        List<String> buttonOptions = quizViewModel.getButtonOptions().getValue();
+        buttonOptions.clear();
+
+        MutableLiveData<DogEntity> dogPicked = quizViewModel.getDogPicked();
+
+        DogEntity dog = dogPicked.getValue();
+
+        Random random = new Random();
+
+
+        while (buttonOptions.size() != 2) {
+            int randomIndex = random.nextInt(dogs.size());
+            DogEntity randomDog = dogs.get(randomIndex);
+
+            if (randomDog != dog && !buttonOptions.contains(randomDog.getImageText())) {
+                String imageText = randomDog.getImageText();
+                buttonOptions.add(imageText);
+            }
+        }
+
+    }
+
+    public void addTextToButtons() {
+
+        MutableLiveData<DogEntity> dogPicked = quizViewModel.getDogPicked();
+
+        DogEntity dog = dogPicked.getValue();
+
+        List<String> buttonOptions = quizViewModel.getButtonOptions().getValue();
+
+        List<String> shuffeledTexts = new ArrayList<>();
+
+        shuffeledTexts.add(dog.getImageText());
+        shuffeledTexts.add(buttonOptions.get(0));
+        shuffeledTexts.add(buttonOptions.get(1));
+
+        Collections.shuffle(shuffeledTexts);
+
+        button.setText(shuffeledTexts.get(0));
+        button2.setText(shuffeledTexts.get(1));
+        button3.setText(shuffeledTexts.get(2));
+    }
+
+    public static void pickRandomDog(List<DogEntity> dogs) { //Sets the value of dogPicked
+
+
+        DogEntity dog;
+        if (dogs == null || dogs.isEmpty()) {
+            return;
+        }
+
+        Log.d("quizDebug", "dogs: " + dogs.size());
+
+
+        Random random = new Random();
+        int randomIndex = random.nextInt(dogs.size());
+
+        /* Causes the app to crash
+        do {
+            dog = dogs.get(randomIndex);
+        } while (dog.equals(dogPicked.getValue()));
+*/
+        dog = dogs.get(randomIndex);
+
+        Log.d("quizDebug", "dogPicked: " + dog.getImageText());
+
+        MutableLiveData<DogEntity> dogPicked = new MutableLiveData<>(dog);
+
+        quizViewModel.setDogPicked(dogPicked);
+
+
+    }
+
+    public void play() {
+
+        MutableLiveData<DogEntity> dogPicked = quizViewModel.getDogPicked();
+
+        DogEntity dog = dogPicked.getValue();
+
+        if (dog.getImageResource() != 0) {
+            //  dogimageView.setImageResource(dog.getImageResource()); //setImageResource reads from disk and will bottleneck the app
+            Glide.with(this).load(dog.getImageResource()).into(dogImageView); // use glide to load image from resource async
+        } else if (dog.getImageUri() != null) {
+            // dogimageView.setImageURI(imageUri); // seImageUri reads from disk and will bottleneck the app
+            Glide.with(this).load(dog.getImageUri()).into(dogImageView); // use glide to load image from uri async
+        } else {
+            Log.d("test", "No image URI or imageResource");
+        }
+
+        addTextToButtons();
 
     }
 
--- app/src/main/java/com/example/oblig1quizapp/QuizViewModel.java
+++ app/src/main/java/com/example/oblig1quizapp/QuizViewModel.java
@@ -1,5 +1,8 @@
 package com.example.oblig1quizapp;
 
+import static com.example.oblig1quizapp.QuizActivity.fillButtonOptionsList;
+import static com.example.oblig1quizapp.QuizActivity.pickRandomDog;
+
 import android.app.Application;
 import android.content.Context;
 import android.net.Uri;
@@ -24,6 +27,7 @@ public class QuizViewModel extends AndroidViewModel {
     private DogRepository mRepository;
 
     private final LiveData<List<DogEntity>> mAllDogs;
+
 
     private MutableLiveData<Integer> score = new MutableLiveData<>(0);
     private MutableLiveData<Integer> roundsplayed = new MutableLiveData<>(1);
@@ -62,12 +66,12 @@ public class QuizViewModel extends AndroidViewModel {
     }
 
 
-    public LiveData<Integer> getScore() {
+    public MutableLiveData<Integer> getScore() {
         return score;
     }
 
 
-    public LiveData<Integer> getRoundsplayed() {
+    public MutableLiveData<Integer> getRoundsplayed() {
         return roundsplayed;
     }
 
@@ -77,169 +81,15 @@ public class QuizViewModel extends AndroidViewModel {
     }
 
 
-    public void play(ImageView dogimageView, Button button1, Button button2, Button button3, Context context) {
-
-        DogEntity dog = dogPicked.getValue();
-
-        if (dog.getImageResource() != 0) {
-            //  dogimageView.setImageResource(dog.getImageResource()); //setImageResource reads from disk and will bottleneck the app
-            Glide.with(context).load(dog.getImageResource()).into(dogimageView); // use glide to load image from resource async
-        } else if (dog.getImageUri() != null) {
-            // dogimageView.setImageURI(imageUri); // seImageUri reads from disk and will bottleneck the app
-            Glide.with(context).load(dog.getImageUri()).into(dogimageView); // use glide to load image from uri async
-        } else {
-            Log.d("test", "No image URI or imageResource");
-        }
-
-        addTextToButtons(button1, button2, button3);
-
+    public MutableLiveData<DogEntity> getDogPicked() {
+        return dogPicked;
     }
 
 
-    public void pickRandomDog(List<DogEntity> dogs) { //Sets the value of dogPicked
-
-
-        DogEntity dog;
-        if (dogs == null || dogs.isEmpty()) {
-            return;
-        }
-
-        Log.d("quizDebug", "dogs: " + dogs.size());
-
-
-        Random random = new Random();
-        int randomIndex = random.nextInt(dogs.size());
-
-        /* Causes the app to crash
-        do {
-            dog = dogs.get(randomIndex);
-        } while (dog.equals(dogPicked.getValue()));
-*/
-        dog = dogs.get(randomIndex);
-
-        Log.d("quizDebug", "dogPicked: " + dog.getImageText());
-        dogPicked.setValue(dog);
-
+    public void setDogPicked(MutableLiveData<DogEntity> dogPicked) {
+        this.dogPicked = dogPicked;
     }
 
-    public void fillButtonOptionsList(List<DogEntity> dogs) {
-
-
-        List<String> buttonOptions = getButtonOptions().getValue();
-        buttonOptions.clear();
-
-        DogEntity dog = dogPicked.getValue();
-
-        Random random = new Random();
-
-
-        while (buttonOptions.size() != 2) {
-            int randomIndex = random.nextInt(dogs.size());
-            DogEntity randomDog = dogs.get(randomIndex);
-
-            if (randomDog != dog && !buttonOptions.contains(randomDog.getImageText())) {
-                String imageText = randomDog.getImageText();
-                buttonOptions.add(imageText);
-            }
-        }
-
-    }
-
-    public void addTextToButtons(Button button1, Button button2, Button button3) {
-
-
-        DogEntity dog = dogPicked.getValue();
-
-        List<String> buttonOptions = getButtonOptions().getValue();
-
-        List<String> shuffeledTexts = new ArrayList<>();
-
-        shuffeledTexts.add(dog.getImageText());
-        shuffeledTexts.add(buttonOptions.get(0));
-        shuffeledTexts.add(buttonOptions.get(1));
-
-        Collections.shuffle(shuffeledTexts);
-
-        button1.setText(shuffeledTexts.get(0));
-        button2.setText(shuffeledTexts.get(1));
-        button3.setText(shuffeledTexts.get(2));
-    }
-
-
-    //Compare the text of the dog on the screen to the text of the button clicked
-    public void checkAnswer(Button button, TextView answerView) {
-
-
-        DogEntity dog = dogPicked.getValue();
-
-        if (button.getText().equals(dog.getImageText())) {
-            answerView.setText("Correct Answer!");
-
-            int currentScore = score.getValue() != null ? score.getValue() : 0;
-            score.setValue(currentScore + 1);
-            Log.d("quizDebug", "Score: " + currentScore);
-
-        } else {
-            answerView.setText("The correct answer was: " + dog.getImageText());
-        }
-    }
-
-    public void playAgain(ImageView dogImageView, Button button1, Button button2, Button button3, List<DogEntity> dogs, Context context) {
-
-
-        pickRandomDog(dogs);
-
-
-        DogEntity dog = dogPicked.getValue();
-
-        //update Rounds played
-        int roundsPlayed = roundsplayed.getValue() != null ? roundsplayed.getValue() : 1;
-        roundsplayed.setValue(roundsPlayed + 1);
-        Log.d("quizDebug", "Rounds played: " + roundsPlayed);
-
-        //Set the Dog image
-        if (dog.getImageResource() != 0) {
-            //  dogImageView.setImageResource(dog.getImageResource()); //setImageResource reads from disk and will bottleneck the app
-            Glide.with(context).load(dog.getImageResource()).into(dogImageView); // use glide to load image from resource async
-        } else if (dog.getImageUri() != null) {
-            //  dogImageView.setImageURI(imageUri);  // seImageUri reads from disk and will bottleneck the app
-            Glide.with(context).load(dog.getImageUri()).into(dogImageView); // use glide to load image from uri async
-        } else {
-            Log.d("test", "No image URI or imageResource");
-        }
-
-
-        //Update the buttons
-        fillButtonOptionsList(dogs);
-        addTextToButtons(button1, button2, button3);
-
-    }
-
-    public void setButtonClickListeners(List<DogEntity> dogs, Button button, Button button2, Button button3, TextView answerView, ImageView dogImageView, Context context) {
-
-
-        button.setOnClickListener(v ->
-        {
-
-            checkAnswer(button, answerView);
-            playAgain(dogImageView, button, button2, button3, dogs, context);
-
-        });
-
-        button2.setOnClickListener(v ->
-        {
-            checkAnswer(button2, answerView);
-            playAgain(dogImageView, button, button2, button3, dogs, context);
-
-        });
-
-        button3.setOnClickListener(v ->
-        {
-            checkAnswer(button3, answerView);
-            playAgain(dogImageView, button, button2, button3, dogs, context);
-
-        });
-    }
 
 }
 
